8dd48480b233c6810fb26c64a6a25a672cf0298e
mzw4 <mzw4@cornell.edu>
2015-03-05 16:57:21 -0500

    added requirements and basic eb app

--- p2/eb_app/app.py
+++ p2/eb_app/app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
+  #Set application.debug=true to enable tracebacks on Beanstalk log output. 
+  #Make sure to remove this line before deploying to production.
   app.debug = True
   app.run()
